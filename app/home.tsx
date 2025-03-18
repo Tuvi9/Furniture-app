9e8ef3115b3673eb7a3891e4ea24ab0528c441d1
Tuvi9 <kaspupavel@gmail.com>
2025-03-18 23:28:52 +0200

    feat: Backend connection established

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,20 @@
 import { View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Home () {
+    const backAdress = process.env.EXPO_PUBLIC_IPADDRESS
+    const [greeting, setGreeting] = useState('Loading...')
+
+    useEffect(() => {
+        axios.get(`${backAdress}/api/hello`)
+            .then(response => setGreeting(response.data))
+            .catch(err => console.error(err))
+    }, [])
+
     return(
         <View className='flex w-full h-screen justify-center items-center'>
-            <Text className='text-2xl text-seablue'>Welcome!</Text>
+            <Text className='text-2xl text-seablue'>{greeting}</Text>
         </View>
     )
 }
